8532d72cd39d8d3bb572677ffe8b549be3112fe2
yubeen <hyubin1270@gmail.com>
2019-10-02 15:57:34 +0900

    dd

--- devFw/src/main/java/project/hm/s0002/vo/S0002VO.java
+++ devFw/src/main/java/project/hm/s0002/vo/S0002VO.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component("s0002VO")
 public class S0002VO {
-	private String id = "";
+	private String id;
 	private String pwd = "";
 	private String name = "";
 	private String email = "";
@@ -22,7 +22,7 @@ public class S0002VO {
 	}
 
 	public S0002VO() {
-		System.out.println("MemberVO »ý¼ºÀÚ È£Ãâ!!!!!!!!");
+		System.out.println("MemberVO ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ È£ï¿½ï¿½!!!!!!!!");
 	}
 
 	public String getId() {
