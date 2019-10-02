05f4004db61c04c0c570338cdc9fa92257080683
mming <alswn2868@naver.com>
2019-10-02 15:47:00 +0900

    1111111

--- devFw/src/main/java/project/hm/p0001/controller/P0001ControllerImpl.java
+++ devFw/src/main/java/project/hm/p0001/controller/P0001ControllerImpl.java
@@ -38,6 +38,7 @@ public class P0001ControllerImpl implements P0001Controller {
 	P0001Service p0001Service;
 	@Autowired
 	P0001VO p0001VO;
+	int n;
 	
 	@Override
 	@RequestMapping(value = "/hm/p0001/searchInit.do", method = { RequestMethod.GET, RequestMethod.POST })
