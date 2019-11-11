c7aa0740ef57f76793e1700175b3170596382817
wpsl7 <wpsl7@DESKTOP-GLTT0JK>
2019-11-11 17:50:04 +0900

    ibsheet회원관리추가

--- devFw/src/main/java/project/Customers/P004/D001/dao/C_P004_D001DAO.java
+++ devFw/src/main/java/project/Customers/P004/D001/dao/C_P004_D001DAO.java
@@ -1,5 +1,20 @@
 package project.Customers.P004.D001.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.dao.DataAccessException;
+
+import project.Customers.P004.D001.vo.C_P004_D001VO;
+
+
+
 public interface C_P004_D001DAO {
+	 public List<C_P004_D001VO> searchList(Map<String, Object> searchMap) throws DataAccessException;
+
+		public void updateData(Map<String, String> row) throws DataAccessException;
+
+		public void deleteData(Map<String, String> row) throws DataAccessException;
+		 
 
 }
--- devFw/src/main/java/project/Customers/P004/D001/service/C_P004_D001Service.java
+++ devFw/src/main/java/project/Customers/P004/D001/service/C_P004_D001Service.java
@@ -1,5 +1,14 @@
 package project.Customers.P004.D001.service;
 
-public interface C_P004_D001Service {
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.dao.DataAccessException;
+
+import project.Customers.P004.D001.vo.C_P004_D001VO;
+
+
+public interface C_P004_D001Service {
+	 public List<C_P004_D001VO> searchList(Map<String, Object> searchMap) throws DataAccessException;
+	 public void saveData(Map<String, String[]> dataMap)  throws DataAccessException ;
 }
