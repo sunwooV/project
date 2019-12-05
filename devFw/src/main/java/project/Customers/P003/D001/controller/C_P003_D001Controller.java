7bae8ac604f773ac0098fd8311dfa486553d79da
sunwooV <sws_221@naver.com>
2019-12-05 15:03:03 +0900

    상품 Q&A 오류 수정/판매자 등록 하는 중

---
+++ devFw/src/main/java/project/Customers/P003/D001/controller/C_P003_D001Controller.java
@@ -0,0 +1,10 @@
+package project.Customers.P003.D001.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+
+public interface C_P003_D001Controller {
+	public ModelAndView enrollSeller(HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
---
+++ devFw/src/main/java/project/Customers/P003/D001/vo/C_P003_D001VO.java
@@ -0,0 +1,8 @@
+package project.Customers.P003.D001.vo;
+
+import org.springframework.stereotype.Component;
+
+@Component("C_P003_D001VO")
+public class C_P003_D001VO {
+
+}
